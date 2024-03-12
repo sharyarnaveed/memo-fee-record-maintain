4f32c61baf367553f94b1fe9be234db796ec2974
sharyar <131683156+sharyarnaveed@users.noreply.github.com>
2024-03-12 21:59:25 +0500

    version 4

---
+++ forgot_password.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="css/sign_in.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <title>Forgot Password</title>
+</head>
+<body>
+    <section class="forgot_container">
+        <section class="forgot_mini">
+        <div class="forgot_logo"><img src="resources/images/forgot.png" alt=""></div>
+        <div class="forgot_email">
+            <h3>Password Reset</h3>
+            <form action="Post" class="email_form">
+                <input type="email"class="input_email_forgot" name="" placeholder="Email.." id="">
+                <input type="submit" class="forgot_submit" value="Reset">
+            </form>
+        </div>
+        </section>
+    </section>
+</body>
+</html>
